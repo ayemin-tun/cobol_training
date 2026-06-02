bc80418f92a60dcf94101eefd19cb59958d87b90
Aye Min Tun <amtamt547@gmail.com>
2026-06-03 00:28:55 +0630

    format folder structure

--- test.cbl
+++
@@ -1,9 +0,0 @@
-       identification division.
-       program-id. test_progam.
-       data division.
-       working-storage section.
-       procedure division.
-       main-procedure.
-            display "Hello World".
-            stop run.
-       end program test_progam.
